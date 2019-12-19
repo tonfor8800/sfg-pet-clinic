4683c0dfa95b7fb1259baf328d1bd4ee9f956268
tony.forbes <tonyforbes88@icloud.com>
2019-12-18 23:35:54 -0600

    Adding POJOs. Closes #2.

---
+++ src/main/java/com/forbes/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.forbes.springframework.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/forbes/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.forbes.springframework.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
