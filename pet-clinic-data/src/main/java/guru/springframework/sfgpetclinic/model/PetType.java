fe2a2468da24ce2929f5a3c61f154770d967e45b
tony.forbes <tonyforbes88@icloud.com>
2019-12-21 22:49:56 -0600

    Refactoring to common crud interface. closes #10

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -1,13 +1,12 @@
 package guru.springframework.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
